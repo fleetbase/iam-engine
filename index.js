a49b8a5d2e050ce5c4b0097b921fc83b97e59dd3
Ronald A. Richardson <me@ron.dev>
2025-10-15 12:04:33 +0800

    v0.1.4 ~ upgraded dependencies

--- index.js
+++ index.js
@@ -6,6 +6,30 @@ const Funnel = require('broccoli-funnel');
 module.exports = buildEngine({
     name,
 
+    init() {
+        if (this._super.init) this._super.init.apply(this, arguments);
+
+        for (let addon of this.addons || []) {
+            if (['@fleetbase/ember-core', '@fleetbase/ember-ui'].includes(addon.name)) {
+                let origTreeFor = addon.treeFor?.bind(addon);
+                addon.treeFor = function (type) {
+                    if (type === 'styles') {
+                        return undefined;
+                    }
+                    return origTreeFor ? origTreeFor(type) : undefined;
+                };
+            }
+        }
+
+        const origNonDup = this.nonDuplicatedAddonInvoke?.bind(this);
+        this.nonDuplicatedAddonInvoke = (hook, args = []) => {
+            if (hook === 'treeFor' && args[0] === 'styles') {
+                return []; // prevents dependency style relocation funnel
+            }
+            return origNonDup ? origNonDup(hook, args) : [];
+        };
+    },
+
     postprocessTree(type, tree) {
         if (type === 'css') {
             tree = new Funnel(tree, {
